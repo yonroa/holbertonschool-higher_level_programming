12b4dd7515071f6a8f4f9295ef3b74a7c5cefce5
yonroa <yonanderson11@gmail.com>
2022-05-03 17:29:30 -0500

    Upload Tasks

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * insert_node - Inserts a number into a sorted singly linked list
+ * @head: First node
+ * @number: Number to be inserted
+ *
+ * Return: The address of the new node, NUll if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp = *head;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	if (*head == NULL)
+		return (*head = new);
+	while (tmp)
+	{
+		if (tmp->next->n >= number || tmp->next == NULL)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for h in range(i + 1, 10):
+        if i == 8:
+            print(f"{i}{h}")
+        else:
+            print(f"{i}{h}", end = ", ")
