bb20a3ffb8e72a4723e63d5a0b159a0623a3a11a
yonroa <yonanderson11@gmail.com>
2022-05-05 18:23:15 -0500

    Upload tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{num_args} arguments:")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+*Learning about modules in python*
