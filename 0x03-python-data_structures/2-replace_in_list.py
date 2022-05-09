7c1ae35ec63bfdc8b906460907edc6548e450a04
yonroa <yonanderson11@gmail.com>
2022-05-09 09:44:40 -0500

    Task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    elif len(my_list) < idx:
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
