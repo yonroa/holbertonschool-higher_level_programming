9fcc3f368c5331adb7eaa5861618e4e138881160
yonroa <yonanderson11@gmail.com>
2022-05-10 15:59:55 -0500

    Fix task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) != str:
         return (0)
     sums = []
     romans = {'I': 1, 'V': 5, 'X': 10,
