aeac84d4cf3d85940d65ee91cfe0fe9b4a23bb23
yonroa <yonanderson11@gmail.com>
2022-05-10 16:14:59 -0500

    Fix 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    return (max(a_dictionary, key=lambda x: a_dictionary[x]))
+    maximus = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == maximus:
+            return (key)
