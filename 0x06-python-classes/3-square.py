c7d5fb7a41045bd70590285494ecf1e7457902a5
yonroa <yonanderson11@gmail.com>
2022-05-24 16:27:08 -0500

    Add task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """A simple class without methods or public attributes"""
+
+    def __init__(self, size=0):
+        """Instantiation safe of an square"""
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """int: Return the area of the square"""
+        return (self.__size ** 2)
