3c2795c53a657bd543b9b38ac8ee56f8b56d8237
yonroa <yonanderson11@gmail.com>
2022-05-24 17:12:45 -0500

    Add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """A simple class without methods or public attributes"""
+
+    def __init__(self, size=0):
+        """Instantiation safe of an square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: Return the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the attribute size"""
+        if type(value) == int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """int: Return the area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square with the char '#'"""
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
