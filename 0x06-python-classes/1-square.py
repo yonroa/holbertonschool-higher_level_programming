c67c6088483e1685427711aa1564f30c6596db2f
yonroa <yonanderson11@gmail.com>
2022-05-24 15:45:27 -0500

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """A simple class without methods or public attributes.
+
+    """
+
+    def __init__(self, size):
+        """Instantiation of an `square`.
+
+        Args:
+            size (:obj:`int`, optional): Size of the `square`.
+
+        """
+        self.__size = size
