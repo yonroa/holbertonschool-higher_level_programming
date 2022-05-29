9391b8e0222272fcd18f9e092f08359631acab8d
yonroa <yonanderson11@gmail.com>
2022-05-29 16:53:02 -0500

    Update task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,31 +10,11 @@ This module supplies one function, add_integer(). For example.,
 
 
 def add_integer(a, b=98):
-    """Return a + b.
+    """Return a + b."""
 
-    Works whit integers and floats:
-
-    >>> add_integer(8, -3)
-    5
-
-    >>> add_integer(37.5, -7.3)
-    30
-
-    a and b must be numbers:
-
-    >>> add_integer(4, "hello")
-    Traceback (most recent call last):
-    ...
-    TypeError: b must be an integer
-
-    >>> add_integer(True, -8)
-    Traceback (most recent call last):
-    ...
-    TypeError: a must be an integer
-    """
-    if type(a) is not int and type(a) is not float:
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
+    elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
