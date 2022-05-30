b5547d9781d0bdb191f08e574f63386f3544bcf7
yonroa <yonanderson11@gmail.com>
2022-05-30 12:43:54 -0500

    Update tasks

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The "4-print_square" module
+
+This module supplies one function, print_square(). For example.,
+
+>>> print_square(3)
+###
+###
+###
+"""
+
+
+def print_square(size):
+    """Print a square with the character '#'
+    Print some error if something is wrong
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print('#', end="")
+        print("")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
