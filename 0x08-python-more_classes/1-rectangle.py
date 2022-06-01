a0ec03aaee8123b6551d3305df470d140b6a6193
yonroa <yonanderson11@gmail.com>
2022-05-31 22:07:29 -0500

    Update task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Instantiation safe of an rectangle"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
