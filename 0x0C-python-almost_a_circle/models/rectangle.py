85f49e0e3f102bd871f517cce630b7e36178056a
yonroa <yonanderson11@gmail.com>
2022-06-14 16:35:20 -0500

    Update tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,6 +91,8 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """assigns an argument to each attribute"""
         if args is not None and len(args) != 0:
+            if type(args[0]) is not int and args[0] is not None:
+                raise ValueError
             if len(args) > 0:
                 self.id = args[0]
             if len(args) > 1:
