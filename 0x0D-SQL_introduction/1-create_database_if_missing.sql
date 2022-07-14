7fc2cf3c4b834fa9111c747c21738742e55a331d
yonroa <yonanderson11@gmail.com>
2022-07-13 20:03:01 -0500

    Upload all mandatory tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creates the database hbtn_0c_0 in your MySQL server
+-- Only if database hbtn_0c_0 does not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database hbtn_0c_0 in your MySQL server
+-- Only if database hbtn_0c_0 exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
