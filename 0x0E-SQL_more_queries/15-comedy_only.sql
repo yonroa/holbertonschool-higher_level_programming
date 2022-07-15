8e1dfe2d514e1bdb63cd5537a097998c0599dea1
yonroa <yonanderson11@gmail.com>
2022-07-14 22:39:14 -0500

    Upload files with the tasks

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT title FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = "Comedy"
+GROUP BY title
+ORDER BY title ASC;
