2ee5ea63c973be7ffac68fd84bb39d12c3d3402a
yonroa <yonanderson11@gmail.com>
2022-08-30 23:04:31 -0500

    Update tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
     cur = db.cursor()
     cur.execute(
         f"SELECT * FROM states WHERE\
