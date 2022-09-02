7b1e33825bfc2e794d833e56f3c24a2139879c9f
yonroa <yonanderson11@gmail.com>
2022-09-02 17:21:02 -0500

    Fix pep8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,8 +4,8 @@
 if __name__ == "__main__":
     import requests
     import sys
-    r = requests.get(
-        'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
     commits = r.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
