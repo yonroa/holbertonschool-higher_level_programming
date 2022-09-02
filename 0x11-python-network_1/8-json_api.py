65fdd6ffda35658c622fc44acbaf125e7be6270f
yonroa <yonanderson11@gmail.com>
2022-09-02 17:02:23 -0500

    Fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     import requests
     import sys
     payload = {'q': ""}
-    if len(sys.argv) == 3:
-        payload = {'q': sys.argv[2]}
+    if len(sys.argv) == 2:
+        payload = {'q': sys.argv[1]}
     r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     try:
         r_dict = r.json()
